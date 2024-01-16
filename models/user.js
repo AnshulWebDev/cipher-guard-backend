c054c07853c944ed576244ebe49db2fe22fa41f2
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-16 19:50:09 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -12,7 +12,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    trim: true,
   },
   isEmailVerify: {
     type: Boolean,
@@ -66,7 +65,6 @@ const userSchema = new mongoose.Schema({
   emergencyMail: {
     type: String,
     trim: true,
-    unique: true,
   },
   emergencyAccessPasswd: {
     type: String,
