c5138757d0fb59a41d0d827d26aad100ea4316fa
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-16 19:47:50 +0530

    Update verifyOtp.js

--- controllers/auth/register/verifyOtp.js
+++ controllers/auth/register/verifyOtp.js
@@ -9,8 +9,7 @@ exports.verifyOtp = async (req, res) => {
   try {
     const { otp } = req.body;
     const cookiesValue =
-      req.cookies.data ||
-      req.header("Authorization").replace("Bearer ", "");
+      req.cookies.data || req.header("Authorization").replace("Bearer ", "");
     if (!cookiesValue) {
       Response(res, false, "Please register first", 422);
       return;
@@ -34,7 +33,12 @@ exports.verifyOtp = async (req, res) => {
       Response(res, false, "Invalid otp", 422);
       return;
     }
-    try {
+    const isUserAlreadyRegister = await user.findOne({ email: decode.email });
+    if (isUserAlreadyRegister) {
+      //check if user is already registered or not
+      Response(res, false, "Email is already registered", 422);
+      return;
+    } else {
       await user.create({
         firstName: decode.firstName,
         lastName: decode.lastName,
@@ -47,107 +51,103 @@ exports.verifyOtp = async (req, res) => {
         decode.email,
         "Welcome to CipherGuard",
         `<!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Welcome to SecurePass Pro</title>
-          <style>
-            /* Add your styles here */
-            body {
-              font-family: 'Arial', sans-serif;
-              background-color: #f4f4f4;
-              margin: 0;
-              padding: 0;
-            }
-        
-            .container {
-              max-width: 600px;
-              margin: 20px auto;
-              background-color: #f4f4f4;
-              padding: 20px;
-              border-radius: 8px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            }
-        
-            h1 {
-              color: #262626;
-            }
-        
-            p {
-              font-size: 16px;
-              line-height: 1.5;
-              color: #262626;
-            }
-        
-            ul {
-              list-style-type: none;
-              padding: 0;
-            }
-        
-            ul li::before {
-              content: "✓ ";
-              color: #262626;
-            }
-        
-            .footer {
-              border-top: 2px solid #ccc;
-              padding-top: 20px;
-              margin-top: 20px;
-            }
-        
-            /* Responsive Styles */
-            @media screen and (max-width: 600px) {
-              .container {
-                padding: 15px;
+          <html lang="en">
+          
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Welcome to SecurePass Pro</title>
+            <style>
+              /* Add your styles here */
+              body {
+                font-family: 'Arial', sans-serif;
+                background-color: #f4f4f4;
+                margin: 0;
+                padding: 0;
               }
-            }
-          </style>
-        </head>
-        
-        <body>
-        
-          <div class="container">
-            <h1>${decode.firstName}, Welcome to CipherGuard!</h1>
-            <p>You are now part of the CipherGuard family! Get ready to depart on an exciting journey with us!</p>
-            <p>To make things extra special for you, starting today, we will send you a series of exclusive emails with
-              amazing tips and tricks to get the most out of.</p>
-            <p>Get ready!</p>
-        
-            <h2>Key Features:</h2>
-            <ul>
-              <li>Generate unique Passwords</li>
-              <li>No forgot password policy</li>
-              <li>Login Alert!</li>
-              <li>Username Generator option</li>
-              <li>Password Storage and Encryption</li>
-              <li>Secure Notes</li>
-              <li>After four unsuccessful password attempts, the account may be temporarily suspended for security purposes</li>
-              <li>Emergency Access</li>
-              <li>login alert (if user think there is suspicious login they can temporarily blocked their account)</li>
-              <li>Vault password</li>
-            </ul>
-        
-            <div class="footer">
-              <p>Best,<br><a href="https://devglimpse.com" target="_blank">The CipherGuard team</a></p>
+          
+              .container {
+                max-width: 600px;
+                margin: 20px auto;
+                background-color: #f4f4f4;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              }
+          
+              h1 {
+                color: #262626;
+              }
+          
+              p {
+                font-size: 16px;
+                line-height: 1.5;
+                color: #262626;
+              }
+          
+              ul {
+                list-style-type: none;
+                padding: 0;
+              }
+          
+              ul li::before {
+                content: "✓ ";
+                color: #262626;
+              }
+          
+              .footer {
+                border-top: 2px solid #ccc;
+                padding-top: 20px;
+                margin-top: 20px;
+              }
+          
+              /* Responsive Styles */
+              @media screen and (max-width: 600px) {
+                .container {
+                  padding: 15px;
+                }
+              }
+            </style>
+          </head>
+          
+          <body>
+          
+            <div class="container">
+              <h1>${decode.firstName}, Welcome to CipherGuard!</h1>
+              <p>You are now part of the CipherGuard family! Get ready to depart on an exciting journey with us!</p>
+              <p>To make things extra special for you, starting today, we will send you a series of exclusive emails with
+                amazing tips and tricks to get the most out of.</p>
+              <p>Get ready!</p>
+          
+              <h2>Key Features:</h2>
+              <ul>
+                <li>Generate unique Passwords</li>
+                <li>No forgot password policy</li>
+                <li>Login Alert!</li>
+                <li>Username Generator option</li>
+                <li>Password Storage and Encryption</li>
+                <li>Secure Notes</li>
+                <li>After four unsuccessful password attempts, the account may be temporarily suspended for security purposes</li>
+                <li>Emergency Access</li>
+                <li>login alert (if user think there is suspicious login they can temporarily blocked their account)</li>
+                <li>Vault password</li>
+              </ul>
+          
+              <div class="footer">
+                <p>Best,<br><a href="https://devglimpse.com" target="_blank">The CipherGuard team</a></p>
+              </div>
             </div>
-          </div>
-        
-        </body>
-        
-        </html>
-        `
+          
+          </body>
+          
+          </html>
+          `
       );
       nodeCache.del("allRegisterUser");
       return res.clearCookie("data").status(200).json({
         success: true,
         message: "Account create successfully",
-      });
-    } catch (error) {
-      //check if user is already registered or not
-      Response(res, false, "Email is already registered", 422);
-      return;
+      })
     }
   } catch (error) {
     console.log(error.message);
