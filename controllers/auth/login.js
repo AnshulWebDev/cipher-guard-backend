decb86a446b90065714693c50eec8ff139753868
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-16 09:37:27 +0530

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -172,7 +172,7 @@ exports.login = async (req, res) => {
       users.token = token;
       await users.save();
       const options = {
-        httpOnly: true,
+        httpOnly: false,
         expires: new Date(Date.now() + 4 + 24 * 60 * 60 * 1000),
       };
       res
