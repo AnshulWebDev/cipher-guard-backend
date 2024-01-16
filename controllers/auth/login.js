2af48afe02593e2b11f31f010947672e5853de1b
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-16 08:38:42 +0530

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -175,13 +175,6 @@ exports.login = async (req, res) => {
         httpOnly: true,
         expires: new Date(Date.now() + 4 + 24 * 60 * 60 * 1000),
       };
-      // Set CORS headers
-      res.header("Access-Control-Allow-Origin", process.env.FRONTENDURL);
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE"
-      );
-      res.header("Access-Control-Allow-Credentials", "true");
       res
         .cookie("token", token, options)
         .status(200)
