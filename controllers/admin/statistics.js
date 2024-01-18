5ce16e739767c4c01d164b65ebb377c32d802004
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-18 19:16:17 +0530

    Update statistics.js

--- controllers/admin/statistics.js
+++ controllers/admin/statistics.js
@@ -1,17 +1,27 @@
 const User = require("../../models/user.js");
+const Notes = require("../../models/secureNotes.js");
+const Password = require("../../models/passwordVault.js");
 const nodeCache = require("../../utils/nodeCache.js");
 const Response = require("../../utils/Response.js");
 
 exports.statistics = async (req, res) => {
   try {
-    const getAllRegisterUser = await User.find();
+    const getAllRegisterUser = await User.find()
+      .sort({ createdAt: -1 })
+      .limit(6);
+    const totalNotes = await Notes.countDocuments();
+    const totalPassword = await Password.countDocuments();
     let allRegisterUser;
     if (nodeCache.has("allRegisterUser")) {
       allRegisterUser = nodeCache.get(JSON.parse("allRegisterUser"));
       Response(res, true, null, 200, allRegisterUser);
       return;
     } else {
-      allRegisterUser = getAllRegisterUser;
+      allRegisterUser = {
+        allUser: getAllRegisterUser,
+        totalNotes: totalNotes,
+        totalPassword: totalPassword,
+      };
       nodeCache.set("allRegisterUser", JSON.stringify(allRegisterUser));
       Response(res, true, null, 200, allRegisterUser);
       return;
