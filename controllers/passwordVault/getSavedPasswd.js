8c65667e8d2813aeb9ba7ee0a82d7ee3df78bafd
AnshulWebDev <anshulvishwa20@gmail.com>
2024-02-28 15:59:22 +0530

    Update getSavedPasswd.js

--- controllers/passwordVault/getSavedPasswd.js
+++ controllers/passwordVault/getSavedPasswd.js
@@ -6,15 +6,16 @@ exports.getSavedPasswd = async (req, res) => {
   try {
     const verifyToken = req.user;
     let getAllSavedPasswd;
-    console.log("nodeCache:", nodeCache);
-    console.log("verifyToken:", verifyToken);
+    // console.log("nodeCache:", nodeCache);
+    // console.log("verifyToken:", verifyToken);
 
     if (nodeCache.has("getSavedPasswd")) {
       getAllSavedPasswd = JSON.parse(nodeCache.get("getSavedPasswd"));
     } else {
-      getAllSavedPasswd = await User.findById(verifyToken.id).populate(
+      const user = await User.findById(verifyToken.id).populate(
         "passwordVault"
       );
+      getAllSavedPasswd = user.passwordVault;
       nodeCache.set("getSavedPasswd", JSON.stringify(getAllSavedPasswd), 300);
     }
     Response(res, true, null, 200, getAllSavedPasswd);
